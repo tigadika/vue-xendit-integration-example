bb79c9634bff49856703cb86dc827015e2aa5dc4
tigadika <amardika.noor@gmail.com>
2023-02-12 21:05:19 +0700

    feat: add cart functionality with remove product from cart

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,57 +1,75 @@
 <template>
-  <div class="w-full h-[32rem] bg-white border rounded-lg shadow-lg overflow-hidden flex flex-col justify-between">
+  <div
+    class="w-full h-[32rem] bg-white border rounded-lg shadow-lg overflow-hidden flex flex-col justify-between"
+  >
     <div class="">
-      <img :src="product.thumbnail" alt="Placeholder image" class="w-full h-[15rem] object-cover">
+      <img
+        :src="product.thumbnail"
+        alt="Placeholder image"
+        class="w-full h-[15rem] object-cover"
+      />
       <div class="px-4 py-3">
-        <div class="font-bold text-xl mb-2">{{ titleTrim }}</div>
+        <div class="text-xl mb-2">{{ titleTrim }}</div>
+        <div class="font-bold text-xl mb-2">${{ product.price }}</div>
         <p class="text-gray-700 text-base">
           {{ descTrim }}
         </p>
       </div>
     </div>
-    <button @click="cartAction" class="mx-auto mb-3 rounded-full bg-black text-white px-10 py-2 hover:bg-gray-800">Add
-      to
-      Cart - ${{ product.price }}</button>
+    <button
+      @click="cartAction"
+      v-if="page === 'HomePage'"
+      class="mx-auto mb-3 rounded-full bg-black text-white px-10 py-2 hover:bg-gray-800"
+    >
+      Add to Cart
+    </button>
+    <button
+      @click="deleteFromCart"
+      v-if="page === 'CartPage'"
+      class="mx-auto mb-3 rounded-full bg-black text-white px-10 py-2 hover:bg-gray-800"
+    >
+      Remove
+    </button>
   </div>
 </template>
 
 <script>
-import { mapActions, mapWritableState } from 'pinia'
-import { useProductStore } from '../stores/product'
+import { mapWritableState } from "pinia";
+import { useProductStore } from "../stores/product";
 
 export default {
   name: "ProductCard",
-  props: [
-    'product'
-  ],
+  props: ["product", "page"],
   computed: {
-    ...mapWritableState(useProductStore, ['cart']),
+    ...mapWritableState(useProductStore, ["cart"]),
     descTrim() {
-      let trimmed = this.product.description.split(" ")
+      let trimmed = this.product.description.split(" ");
       if (trimmed.length > 8) {
-        return trimmed.slice(0, 8).join(" ").concat("... ")
+        return trimmed.slice(0, 8).join(" ").concat("... ");
       } else {
-        return trimmed.join(" ")
+        return trimmed.join(" ");
       }
     },
     titleTrim() {
-      let trimmed = this.product.title.split(" ")
+      let trimmed = this.product.title.split(" ");
       if (trimmed.length > 2) {
-        return trimmed.slice(0, 2).join(" ").concat("... ")
+        return trimmed.slice(0, 2).join(" ").concat("... ");
       } else {
-        return trimmed.join(" ")
+        return trimmed.join(" ");
       }
-    }
+    },
   },
   methods: {
     cartAction() {
-      console.log("cart action", this.cart);
-      this.cart.push(this.product)
-    }
-  }
-}
+      this.cart.push(this.product);
+    },
+    deleteFromCart() {
+      this.cart = this.cart.filter((el) => {
+        return el.title !== this.product.title;
+      });
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -2,16 +2,45 @@
   <div class="flex flex-col h-screen">
     <div class="mx-auto pt-10 flex flex-col w-5/6 space-y-10">
       <div class="mx-auto font-bold tracking-wide text-4xl">Testing Cart</div>
+      <div class="flex flex-row justify-between">
+        <Button
+          @click="goToHome"
+          class="px-3 py-2 leading-tight bg-white text-gray-500 border rounded-lg border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+        >
+          Home
+        </Button>
+      </div>
+      <div class="mx-auto grid grid-cols-5 gap-4 w-full">
+        <ProductCard
+          v-for="product in cart"
+          :key="product.id"
+          :product="product"
+          page="CartPage"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import { mapActions, mapWritableState } from "pinia";
+import ProductCard from "../components/ProductCard.vue";
+import { useProductStore } from "../stores/product";
 
-}
+export default {
+  name: "CartPage",
+  components: {
+    ProductCard,
+  },
+  computed: {
+    ...mapWritableState(useProductStore, ["cart"]),
+  },
+  methods: {
+    goToHome() {
+      this.$router.push("/");
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
